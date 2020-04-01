7ce0eacc695fe141bc80dfd4a3fdeda3952bf423
Marc Finger <marc.finger@gmx.net>
2020-04-01 17:50:04 +0200

    Revert "Merge branch 'init-load' into dev"
    This reverts commit afe07b94d58156a596e6a55b7cc17cf19cd08294.

--- Python/getCOVID-19_germany.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-germanyCurrent = pd.read_csv('https://opendata.arcgis.com/datasets/917fc37a709542548cc3be077a786c17_0.csv')
-
-germanyCurrent.to_csv('C:/Users/Marc/PycharmProjects/COVID-19/Data/covid-19_germany_current.csv', sep=';', encoding='utf-8', index=False)
-
-germanyTimeline = pd.read_csv('https://opendata.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0.csv')
-
-germanyTimeline.to_csv('C:/Users/Marc/PycharmProjects/COVID-19/Data/covid-19_germany_timeline.csv', sep=';', encoding='utf-8', index=False)
